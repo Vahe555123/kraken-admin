6015981f033c6b4c87b69a4f050b6f712c02d0e0
vaheshmavonyan34 <vaheshmavonyan34@gmail.com>
2026-03-22 16:06:40 +0400

    sdfsdf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const base = env.VITE_BASE || '/admin/';
+  const base = '/';
   const proxyTarget = env.VITE_DEV_PROXY_TARGET || 'http://127.0.0.1:3000';
 
   return {
